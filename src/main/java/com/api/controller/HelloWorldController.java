a976b0048a3c3aae1ce9074c98d82bb3c7e412bf
coffeesoftware-admin <rugvedadeshpande1196@gmail.com>
2023-06-02 14:56:15 +0530

    Updted package name

--- src/main/java/com/api/controller/HelloWorldController.java
+++ src/main/java/com/api/controller/HelloWorldController.java
@@ -1,10 +1,10 @@
-package com.javatpoint.controller;
+package com.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javatpoint.service.HelloWorldService;
+import com.api.service.HelloWorldService;
 
 @RestController
 public class HelloWorldController {
--- src/main/java/com/api/service/HelloWorldService.java
+++ src/main/java/com/api/service/HelloWorldService.java
@@ -1,4 +1,4 @@
-package com.javatpoint.service;
+package com.api.service;
 
 import org.springframework.stereotype.Service;
 
